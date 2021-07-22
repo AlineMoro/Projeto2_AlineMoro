b0f95d2d4009b27af1aafe04ccc23ec13abfadf7
Aline Moro <aline_licota@hotmail.com>
2021-07-22 16:25:11 -0300

    Layou1

--- script.js
+++ script.js
@@ -9,12 +9,20 @@ login_btn.addEventListener('click',click_login)
 function click_login() {
     //var res = await axios.post('https://reqres.in/api/login')
     main_text.className = 'main-text-login'
-    text1.innerHTML = '<p>E-mail</p><input type="email" id="email" placeholder="ex:nome_sobrenome@hotmail.com"><p>Senha</p><input type="password" id="senha"></input>'
+    text1.innerHTML = '<h2>Clientes PicPay possuem ofertas exclusivas!</h2><p>E-mail</p><input type="email" class="email" placeholder="ex:nome_sobrenome@hotmail.com"><p>Senha</p><input type="password" class="senha"></input>'
     text1.className = 'text1-login'
     text2.innerHTML = ''
-    text2.className = 'text2-login'
     text3.innerHTML = ''
     div_button.className = 'div-button-login'
     div_button.innerHTML = '<a href="#">Entrar</a>'
+    div_button.parentElement = 'div.text1-login'
+    
+    
+    div_button.addEventListener('click', entrar)
+    function entrar() {
+        //console.log("Entrou")
+        var email = document.querySelector('.email').value,
+            senha = document.querySelector('.senha').value
+    }
 }
 
